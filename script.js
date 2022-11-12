92ae637751879285071aef1c39d0d497509f1cce
tsv-stacks <tsvirdi@yahoo.co.uk>
2022-11-12 00:06:16 +0000

    added password match success

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let p = document.getElementById("password")
 let p2 = document.getElementById("password-confirm")
 let pMsg = document.getElementById("password-message")
+let pMsgSuccess = document.getElementById("password-matched")
 
 pClass = p.classList
 p2Class = p2.classList
@@ -14,17 +15,17 @@ function validatePassword() {
     let errors = []
     let ptest = p.value
     if (ptest.length < 8) {
-        errors.push("Your password must be at least 8 characters.");
+        errors.push("* Your password must be at least 8 characters.");
     }
     if (ptest.search(/[a-z]/i) < 0) {
-        errors.push("Your password must contain at least one letter.");
+        errors.push("* Your password must contain at least one letter.");
     }
     if (ptest.search(/[0-9]/) < 0) {
-        errors.push("Your password must contain at least one digit.");
+        errors.push("* Your password must contain at least one digit.");
     }
 
     if (ptest.search(/[A-Z]/) < 0) {
-        errors.push("Your password must contain at least one capital letter.")
+        errors.push("* Your password must contain at least one capital letter.")
     }
 
     if (errors.length > 0) {
@@ -40,13 +41,14 @@ function passwordMatch() {
     if (p.value !== p2.value) {
         pClass.add("invalid")
         p2Class.add("invalid")
-        pMsg.textContent = "Your passwords do not match."
+        pMsg.textContent = "* Your passwords do not match."
     } else if (p2.value == '') {
         pClass.add("invalid")
         p2Class.add("invalid")
-        pMsg.textContent = "Your password confirmation cannot be blank."
+        pMsg.textContent = "* Your password confirmation cannot be blank."
     } else if (p.value === p2.value) {
         pClass.add("valid")
         p2Class.add("valid")
+        pMsgSuccess.textContent = "* Passwords Match!"
     }
 }
